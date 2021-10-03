d7373b263d5a818707d4f1a00d319a5f1ae6cbd9
Vlad Klunnyy <nfswo93@gmail.com>
2021-10-03 13:01:58 +0300

    Lession 1

---
+++ Lession1/Lession1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lession1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What`s your name?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Hello, {userName}, today, [{DateTime.Now}]");
+        }
+    }
+}

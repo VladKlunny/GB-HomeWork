65e5d89253bef5e65fb48f27f03ffb0b359ac391
Vlad Klunnyy <nfswo93@gmail.com>
2021-11-14 10:37:21 +0300

    Lession3

---
+++ Lession3.3/ConsoleApp4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string s;
+            while ((s = Console.ReadLine()) != null)
+                Console.WriteLine(new string(s.Reverse().ToArray()));
+           
+        }
+    }
+}
